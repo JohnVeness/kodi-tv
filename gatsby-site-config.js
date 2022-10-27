906c39b795b51e738edf378ba284d2d243e8000c
John Veness <john.veness.github@pelago.org.uk>
2022-10-27 19:24:28 +0100

    b

--- gatsby-site-config.js
+++ gatsby-site-config.js
@@ -318,7 +318,7 @@ module.exports = {
       backend: {
         name: "test-repo",
         repo: "JohnVeness/kodi-tv",
-        branch: main,
+        branch: "main",
       },
       site_url: url,
     },
